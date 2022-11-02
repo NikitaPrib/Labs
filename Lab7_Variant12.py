daa7d2020741ec4a3f55260d06db3de0ceef9e32
NikitaPrib <117282484+NikitaPrib@users.noreply.github.com>
2022-11-02 15:49:49 +0300

    Update Lab7_Variant12.py

--- Lab7_Variant12.py
+++ Lab7_Variant12.py
@@ -2,3 +2,9 @@ k = int(input())
 C = int(input())
 a = []
 for i in range(int(input())):
+ a.append(int(input()))
+a.append(0)
+for i in range(len(a)-1, k, -1):
+    a[i] = a[i - 1]
+a[k] = C
+print(a)
